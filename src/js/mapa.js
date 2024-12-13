c8d55c757b39c013b2814d00db4cb143c1c1345d
Abril Guzmán barrera <230350@utxicotepec.edu.mx>
2024-12-12 22:38:07 -0600

    Arreglo

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -6,6 +6,7 @@
     let marker;
 
     //utilizar provider y geocoder
+
     const geocodeService = L.esri.Geocoding.geocodeService();
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -28,6 +29,7 @@
         mapa.panTo(new L.LatLng(posicion.lat, posicion.lng))
 
         //obtener informacion de las calles al soltar el pin
+
         geocodeService.reverse().latlng(posicion, 13).run(function (error, resultado) {
             marker.bindPopup(resultado.address.LongLabel)
 
